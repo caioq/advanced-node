294e550117edaba86e6ef16a5212e1afd46648a5
Caio Queiroz <caio.cesar.g.queiroz@accenture.com>
2021-05-18 20:07:39 -0300

    dns module

---
+++ network/dns.js
@@ -0,0 +1,19 @@
+const dns = require('dns'); // name -- addresses
+
+// dns lookup doest perform any network communication, uses libuv thread
+dns.lookup('google.com', (err, address) => {
+  console.log('lookup', address);
+});
+
+// with network communications
+dns.resolve4('google.com', (err, address) => {
+  console.log('resolve4', address);
+});
+
+// dns.resolve('google.com', 'MX', (err, address) => {
+//   console.log('resolve MX', address);
+// });
+
+dns.reverse('172.217.30.174', (err, hostnames) => {
+  console.log('hostnames ', hostnames);
+})
